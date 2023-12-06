b3a8b1f0f1978e84a9a1dd3a834c79cc52e5babd
Hendrik Maier <hmaier@ipb-halle.de>
2023-12-06 13:13:13 +0100

    modified:   main.go

--- 2023/6/main.go
+++ 2023/6/main.go
@@ -10,56 +10,90 @@ type Race struct {
 var races []Race
 var testRace []Race
 
-func calculateDifferentWays(r Race) []int{
-	var waysToWin []int
-	for speed := 0; speed <= r.time; speed++ {
-		timeToMove := r.time - speed
-		distance := timeToMove * speed
-		// fmt.Printf("Holding down %d seconds, you will travel with a speed of %d for %d miliseconds, to distance of %d. ", speed, speed, timeToMove, distance)
-		if(distance > r.distance){
-			// fmt.Printf("This way you would win! \n")
-			waysToWin = append(waysToWin, speed)	
+// Part 1
+// func calculateDifferentWays(r Race) []int{
+// 	var waysToWin []int
+// 	for speed := 0; speed <= r.time; speed++ {
+// 		timeToMove := r.time - speed
+// 		distance := timeToMove * speed
+// 		// fmt.Printf("Holding down %d seconds, you will travel with a speed of %d for %d miliseconds, to distance of %d. ", speed, speed, timeToMove, distance)
+// 		if(distance > r.distance){
+// 			// fmt.Printf("This way you would win! \n")
+// 			waysToWin = append(waysToWin, speed)
+// 		}else{
+// 			fmt.Printf("\n")
+// 		}
+// 	}
+// 	// fmt.Printf("All ways to win: %#v \n", waysToWin)
+// 	return waysToWin
+
+// }
+
+// Part 2
+
+// Help from: https://www.youtube.com/watch?v=JkfOakgO9YI
+func calculateDifferentWays(r Race) int {
+
+	n := 0
+
+	for hold := 0; hold < r.time; hold++ {
+		timeToMove := r.time - hold
+		distance := timeToMove * hold
+		if distance > r.distance {
+			n += 1
 		}else{
-			fmt.Printf("\n")
+
 		}
 	}
-	// fmt.Printf("All ways to win: %#v \n", waysToWin)
-	return waysToWin
 
+	return n
 }
 
 func main() {
-	// Tests
-	testRace = append(testRace, Race{
-		time:     7,
-		distance: 9,
-	})
 
-	testRace = append(testRace, Race{
-		time:     15,
-		distance: 40,
-	})
-	testRace = append(testRace, Race{
-		time:     30,
-		distance: 200,
-	})
-	// Puzzle
-	races = append(races, Race{
-		time:     47,
-		distance: 400,
-	})
-	races = append(races, Race{
-		time:     98,
-		distance: 1213,
-	})
-	races = append(races, Race{
-		time:     66,
-		distance: 1011,
-	})
-	races = append(races, Race{
-		time:     98,
-		distance: 1540,
-	})
+	longRace := Race{
+		time:     47986698,
+		distance: 400121310111540,
+	}
+	// longRace := Race{
+	// 	time: 71530,
+	// 	distance: 940200,
+	// }
+
+	// Part 2
+	fmt.Printf("Part2: %d \n", calculateDifferentWays(longRace))
+
+	// Tests
+	// testRace = append(testRace, Race{
+	// 	time:     7,
+	// 	distance: 9,
+	// })
+
+	// testRace = append(testRace, Race{
+	// 	time:     15,
+	// 	distance: 40,
+	// })
+	// testRace = append(testRace, Race{
+	// 	time:     30,
+	// 	distance: 200,
+	// })
+	// // Puzzle
+	// races = append(races, Race{
+	// 	time:     47,
+	// 	distance: 400,
+	// })
+	// races = append(races, Race{
+	// 	time:     98,
+	// 	distance: 1213,
+	// })
+	// races = append(races, Race{
+	// 	time:     66,
+	// 	distance: 1011,
+	// })
+	// races = append(races, Race{
+	// 	time:     98,
+	// 	distance: 1540,
+	// })
 
 	// calculateDifferentWays(races[0])
 	// calculateDifferentWays(testRace[0])
@@ -67,25 +101,17 @@ func main() {
 	// calculateDifferentWays(testRace[2])
 
 	// Part 1
-	var everyLen []int
-	for _, elem := range races{
-		everyLen = append(everyLen, len(calculateDifferentWays(elem)))
+	// var everyLen []int
+	// for _, elem := range races{
+	// 	everyLen = append(everyLen, len(calculateDifferentWays(elem)))
 
-	}
-	fmt.Printf("everyLen: %v \n", everyLen)
+	// }
+	// fmt.Printf("everyLen: %v \n", everyLen)
 
-	multiply := 1
-	for _, elem := range everyLen{
-		multiply *= elem
-	}
-	fmt.Printf("Multiply: %v \n", multiply)
-
-	// Part 2
-	fmt.Printf("Part2: %d \n", len(calculateDifferentWays(Race{
-		time: 47986698,
-		distance:400121310111540,
-	})))
-
+	// multiply := 1
+	// for _, elem := range everyLen{
+	// 	multiply *= elem
+	// }
+	// fmt.Printf("Multiply: %v \n", multiply)
 
 }
-
